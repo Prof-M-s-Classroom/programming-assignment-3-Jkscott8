c175a76cdbac12f28f1ee6aacd0203f841643568
Jkscott8 <Jacksonkhscott@gmail.com>
2025-05-07 10:33:09 -0700

    PA3_intialcode

--- graph.h
+++ graph.h
@@ -1,12 +1,60 @@
 #ifndef GRAPH_H
 #define GRAPH_H
+#include "heap.h"
 
 class Graph {
 public:
-    Graph(int vertices);
-    ~Graph();
-    void addEdge(int u, int v, int weight);
-    void primMST();  // Must print MST edges and total weight
+    Graph(int vertices) {
+        numVertices = vertices;
+        adjMatrix = new int*[numVertices];
+        for (int i = 0; i < numVertices; i++) {
+            adjMatrix[i] = new int[numVertices];
+            for (int j = 0; j < numVertices; j++) {
+                adjMatrix[i][j] = INT_MAX;
+            }
+        }
+    }
+    ~Graph() {
+        delete[] adjMatrix;
+    }
+    void addEdge(int u, int v, int weight) {
+        adjMatrix[u][v] = weight;
+        adjMatrix[v][u] = weight;
+    }
+    void primMST() // Must print MST edges and total weight
+    {
+        int weight = 0;
+        std::vector<bool> visited(numVertices, false);
+        std::vector<int> key(numVertices);
+        std::vector<int> weights(numVertices);
+        MinHeap min_heap(numVertices);
+        visited[0] = true;
+        key[0] = 0;
+        weights[0] = 0;
+        for (int k = 1; k < numVertices; k++) {
+            for (int i = 0; i < numVertices-1; i++) {
+                if (visited[i] == true) {
+                    for (int j = 0; j < numVertices; j++) {
+                        if (adjMatrix[i][j] != INT_MAX && visited[j] == false) {
+                            min_heap.insert(j, adjMatrix[i][j]);
+                        }
+                    }
+                }
+            }
+            weights[k] = min_heap.extractMinKey(0);
+            key[k] = min_heap.extractMin();
+            visited[key[k]] = true;
+        }
+        int total_weight = 0;
+        for (int i = 0; i < numVertices; i++) {
+            total_weight += weights[i];
+            std::cout << "Vertex: "<<key[i] <<" Edge-Weight: "<< weights[i] << std::endl;
+        }
+        std::cout << "Total Weight: " << total_weight << std::endl;
+    }
+
+
+
 
 private:
     int** adjMatrix;
--- heap.h
+++ heap.h
@@ -1,15 +1,85 @@
 #ifndef HEAP_H
 #define HEAP_H
+#include <iostream>
+#include <ostream>
 
 class MinHeap {
 public:
-    MinHeap(int capacity);
-    ~MinHeap();
-    void insert(int vertex, int key);
-    int extractMin();
-    void decreaseKey(int vertex, int newKey);
-    bool isInMinHeap(int vertex);
-    bool isEmpty();
+    MinHeap(int capacity) {
+        size = 0;
+        this-> capacity = capacity;
+        heapArray = new int[capacity];
+        keyArray = new int[capacity];
+        position = new int[capacity];
+    }
+
+    ~MinHeap() {
+        delete[] heapArray;
+        delete[] keyArray;
+        delete[] position;
+    }
+
+    void insert(int vertex, int key) {
+        if (size == capacity) {
+            std::cout << "Heap Is Full" << std::endl;
+            return;
+        }
+        size++;
+        int i = size - 1;
+        heapArray[i] = vertex;
+        position[vertex] = i;
+        keyArray[vertex] = key;
+        while (i != 0 && keyArray[heapArray[(i-1)/2]] > keyArray[heapArray[i]]) {
+            int swap = heapArray[(i-1)/2];
+            heapArray[(i-1)/2] = heapArray[i];
+            heapArray[i] = swap;
+            position[heapArray[i]] = i;
+            position[heapArray[(i-1)/2]] = (i-1)/2;
+            i = (i-1)/2;
+        }
+    }
+    int extractMin() {
+        if (size == 0) {
+            std::cout << "Empty Heap" << std::endl;
+            return -1;
+        }
+        int minVal = heapArray[0];
+        heapArray[0] = heapArray[size - 1];
+        position[heapArray[0]] = 0;
+        size--;
+        minHeapify(0);
+        return minVal;
+
+    }
+    int extractMinKey(int vertex) {
+        return keyArray[heapArray[vertex]];
+    }
+    void decreaseKey(int vertex, int newKey) {
+        keyArray[vertex] = newKey;
+        int i = position[vertex];
+        while (i != 0 && keyArray[heapArray[(i-1)/2]] > keyArray[heapArray[i]]) {
+            int swap = heapArray[(i-1)/2];
+            heapArray[(i-1)/2] = heapArray[i];
+            heapArray[i] = swap;
+            position[heapArray[i]] = i;
+            position[heapArray[(i-1)/2]] = (i-1)/2;
+            i = (i-1)/2;
+        }
+
+
+    }
+    bool isInMinHeap(int vertex) {
+        if (heapArray[position[vertex]] == vertex) {
+            return true;
+        }
+        return false;
+    }
+    bool isEmpty() {
+        if (size == 0) {
+            return true;
+        }
+        return false;
+    }
 
 private:
     int* heapArray;        // Heap of vertex indices
@@ -18,7 +88,28 @@ private:
     int capacity;
     int size;
 
-    void minHeapify(int idx);
+    void minHeapify(int idx) {
+        if (size == 0) {
+            return;
+        }
+        int smallest = idx;
+        int left = 2 * idx + 1;
+        int right = 2 * idx + 2;
+        if (left < size && keyArray[heapArray[left]] < keyArray[heapArray[smallest]]) {
+            smallest = left;
+        }
+        if (right < size && keyArray[heapArray[right]] < keyArray[heapArray[smallest]]) {
+            smallest = right;
+        }
+        if (smallest != idx) {
+            int swap = heapArray[smallest];
+            heapArray[smallest] = heapArray[idx];
+            heapArray[idx] = swap;
+            position[heapArray[idx]] = idx;
+            position[heapArray[smallest]] = smallest;
+            minHeapify(smallest);
+        }
+    }
 };
 
 #endif
